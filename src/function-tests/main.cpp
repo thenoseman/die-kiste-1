6a8ce201a9555a398b4799aa141eb7c1be3f1639
thenoseman <frank.schumacher.privat@gmail.com>
2018-02-02 22:21:29 +0100

    add arcade button test

--- src/function-tests/main.cpp
+++ src/function-tests/main.cpp
@@ -10,19 +10,17 @@ extern HardwareSerial Serial;
 #define MATRIX_LED_TYPE NEOPIXEL
 #define MATRIX_BRIGHTNESS 5
 CRGB matrixLeds[MATRIX_NUM_LEDS];
-int fillToRow = 0;
-long unsigned int fillWithColors[] = {
-  CRGB::Red,
-  CRGB::Yellow,
-  CRGB::Blue,
-  CRGB::White
-};
 
 // Potentimeters
+#define POTIS_NUM_POTIS 4
 int potiPins[] = {0,1,2,3};
 int potiActiveValues[] = {0,0,0,0};
 int potiActiveIndex = 0;
 
+// Arcade Switches
+#define AS_NUM_SWITCHES 3
+int arcadeBtnsDin[] = {9, 11, 13};
+int arcadeBtnsLeds[] = {8, 10, 12};
 
 // *** MATRIX ***
 void setupMatrix() {
@@ -55,25 +53,56 @@ void setupPotis() {
 
 // *** POTENTIOMETER ***
 void loopPotis() {
-  #ifdef DEBUG
-    for (unsigned int potiIndex=0; potiIndex < sizeof(potiPins); potiIndex++) {
-      int potiValue = map(analogRead(potiPins[potiIndex]), 0, 1023, 0, 9);
 
-      // If one change is found, skill polling of other potentiometers
-      if (potiValue != potiActiveValues[potiIndex]) {
+  for (int potiIndex=0; potiIndex < POTIS_NUM_POTIS; potiIndex++) {
+    int potiValue = map(analogRead(potiPins[potiIndex]), 0, 1023, 0, 9);
+
+    // If one change is found, skill polling of other potentiometers
+    // TODO: remove bouncing 0->1->0 effect
+    if (potiValue != potiActiveValues[potiIndex]) {
+      #ifdef DEBUG
         Serial.print("Potentiometer #");
         Serial.print(potiIndex);
         Serial.print(" on A");
         Serial.print(potiPins[potiIndex]);
         Serial.print(" = ");
         Serial.println(potiValue);
+      #endif
 
-        // This will be picked up by loopMatrix()
-        potiActiveValues[potiIndex] = potiValue;
-        potiActiveIndex = potiIndex;
-      }
+      // This will be picked up by loopMatrix()
+      potiActiveValues[potiIndex] = potiValue;
+      potiActiveIndex = potiIndex;
     }
-  #endif
+  }
+}
+
+// ARCADE SWITCHES
+void setupArcadeSwitches() {
+  for(int i=0; i < AS_NUM_SWITCHES; i++) {
+    pinMode(arcadeBtnsDin[i], INPUT_PULLUP);
+    pinMode(arcadeBtnsLeds[i], OUTPUT);
+  }
+}
+
+// ARCADE SWITCHES
+void loopArcadeSwitches() {
+  for(int i=0; i < AS_NUM_SWITCHES; i++) {
+    int state = digitalRead(arcadeBtnsDin[i]);
+
+    #ifdef DEBUG
+      Serial.print("AS Switch on D");
+      Serial.print(arcadeBtnsDin[i]);
+      Serial.print(" = ");
+      Serial.print(state);
+    #endif
+
+    #ifdef DEBUG
+      Serial.print(" -> setting to ");
+      Serial.println(state);
+    #endif
+
+    digitalWrite(arcadeBtnsLeds[i], state);
+  }
 }
 
 void setup()
@@ -82,12 +111,15 @@ void setup()
 
   setupMatrix();
   setupPotis();
+  setupArcadeSwitches();
 }
 
 void loop()
 {
   loopMatrix();
   loopPotis();
+  loopArcadeSwitches();
+  delay(50);
 }
 
 
