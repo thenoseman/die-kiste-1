d65cc02dde2e81b33ba71606a79f8213ec202d8d
thenoseman <frank.schumacher.privat@gmail.com>
2018-02-03 22:53:17 +0100

    add pressure gauge

--- src/function-tests/main.cpp
+++ src/function-tests/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include <HardwareSerial.h>
+#include <TimerOne.h>
 
 extern HardwareSerial Serial;
+
+// Common
+#define LED_BRIGHTNESS 5
 
 // Matrix display
 #define MATRIX_PIN 2
 #define MATRIX_NUM_LEDS 100
 #define MATRIX_LED_TYPE NEOPIXEL
-#define MATRIX_BRIGHTNESS 5
 CRGB matrixLeds[MATRIX_NUM_LEDS];
 
 // Potentimeters
@@ -22,11 +25,20 @@ int potiActiveIndex = 0;
 int arcadeBtnsDin[] = {9, 11, 13};
 int arcadeBtnsLeds[] = {8, 10, 12};
 
+// Pressure Gauge
+#define PRESSURE_BTN_PIN 2
+#define PRESSURE_LED_PIN 3
+#define PRESSURE_NUM_LEDS 5
+#define PRESSURE_LED_TYPE NEOPIXEL
+#define PRESSURE_TIMER_MILLIS 100000 // 100 millisecs
+CRGB pressureLeds[PRESSURE_NUM_LEDS];
+volatile int pressurePercent = 0;
+// These colrs are used indicating the "danger" level, starting at 0 (no led lite) to 5 (all lite)
+long unsigned int pressureColors[] = {CRGB::Black, CRGB::LawnGreen, CRGB::Green, CRGB::Yellow, CRGB::Orange, CRGB::Red};
+
 // *** MATRIX ***
 void setupMatrix() {
   FastLED.addLeds<MATRIX_LED_TYPE, MATRIX_PIN>(matrixLeds, MATRIX_NUM_LEDS);
-  FastLED.setBrightness(MATRIX_BRIGHTNESS);
-  FastLED.clear();
 }
 
 // *** MATRIX ***
@@ -43,8 +55,6 @@ void loopMatrix() {
       matrixLeds[i] = CRGB::Yellow;
     }
   }
-
-  FastLED.show();
 }
 
 // *** POTENTIOMETER ***
@@ -105,6 +115,72 @@ void loopArcadeSwitches() {
   }
 }
 
+// PRESSURE GAUGE
+void handleInterruptPressure() {
+  int pressureButtonState = digitalRead(PRESSURE_BTN_PIN);
+  pressureButtonState = pressureButtonState ? LOW : HIGH;
+
+  if (pressureButtonState == HIGH) {
+    pressurePercent+=10;
+  } else {
+    pressurePercent-=10;
+  }
+
+  if (pressurePercent > 100) {
+    pressurePercent = 100;
+  }
+
+  if (pressurePercent < 0) {
+    pressurePercent = 0;
+  }
+
+  #ifdef DEBUG
+    Serial.print("Pressure Gauge Btn = ");
+    Serial.print(pressureButtonState);
+    Serial.print("; pressurePercent = ");
+    Serial.println(pressurePercent);
+  #endif
+}
+
+// PRESSURE GAUGE
+void loopPressureGauge() {
+  int pressurePercentCopy;
+  Serial.println("loopPressureGauge");
+
+  noInterrupts();
+  pressurePercentCopy = pressurePercent;
+  interrupts();
+
+  int pressureToLed = pressurePercentCopy / ( 100 / PRESSURE_NUM_LEDS); 
+  long unsigned int pressureColor = pressureColors[pressureToLed];
+
+  #ifdef DEBUG
+    Serial.print("loopPressureGauge(): pressureToLed = ");  
+    Serial.print(pressureToLed);
+    Serial.print("; pressurePercent = ");
+    Serial.println(pressurePercentCopy);
+  #endif
+
+  for(int i = 0; i < pressureToLed; i++) {
+    pressureLeds[i] = pressureColor;
+  }
+
+  for(int i = pressureToLed; i < PRESSURE_NUM_LEDS; i++) {
+    pressureLeds[i] = CRGB::Black;
+  }
+}
+
+// PRESSURE GAUGE
+void setupPressureGauge() {
+  pinMode(PRESSURE_BTN_PIN, INPUT_PULLUP);
+  pinMode(PRESSURE_LED_PIN, OUTPUT);
+
+  FastLED.addLeds<PRESSURE_LED_TYPE, PRESSURE_LED_PIN>(pressureLeds, PRESSURE_NUM_LEDS);
+ 
+  Timer1.initialize(PRESSURE_TIMER_MILLIS);
+  Timer1.attachInterrupt(handleInterruptPressure);
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -112,6 +188,12 @@ void setup()
   setupMatrix();
   setupPotis();
   setupArcadeSwitches();
+  setupPressureGauge();
+
+  // Finalize FastLED  setup
+  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop()
@@ -119,7 +201,9 @@ void loop()
   loopMatrix();
   loopPotis();
   loopArcadeSwitches();
-  delay(50);
+  loopPressureGauge();
+
+  FastLED.show();
 }
 
 
