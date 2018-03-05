c74ac9bd4428a9d3f8ee40a705bfac9822516d86
thenoseman <frank.schumacher.privat@gmail.com>
2018-03-05 22:01:19 +0100

    use different pin for matrix

--- src/try1/main.cpp
+++ src/try1/main.cpp
@@ -7,27 +7,27 @@
 extern HardwareSerial Serial;
 
 //                                  +-----+
-//                     +------------| USB |-------------+
-//                     |            +-----+             |
-// arcadeBtnsLeds[2]<- | [ ]D13/SCK/noDin   MISO/D12[ ] | -> arcadeBtnsLeds[1]
-//                     | [ ]3.3V            MOSI/D11[ ]~| -> arcadeBtnsLeds[0]
-//                     | [ ]V.ref     ___     SS/D10[ ]~| <- arcadeBtnsDin[2]
-// PRESSURE_LED_PIN <- | [ ]A0       / N \        D9[ ]~| <- arcadeBtnsDin[1]
-// PRESSURE_BTN_PIN -> | [ ]A1      /  A  \       D8[ ] | <- arcadeBtnsDin[0]
-// MATRIX_LED_PIN   <- | [ ]A2      \  N  /       D7[ ] | <- switchboardPins[5] => 64
-//                     | [ ]A3       \_0_/        D6[ ]~| <- switchboardPins[4] => 32
-//     potiPins[0]  -> | [ ]A4/SDA                D5[ ]~| <- switchboardPins[3] => 16
-//     potiPins[1]  -> | [ ]A5/SCL                D4[ ] | <- switchboardPins[2] => 8
-//     potiPins[2]  -> | [ ]A6 (noDIn)       INT1/D3[ ]~| <- switchboardPins[1] => 4
-//     potiPins[3]  -> | [ ]A7 (noDIn)       INT0/D2[ ] | <- switchboardPins[0] => 2
-//                     | [ ]5V                   GND[ ] |
-//                     | [ ]RST                  RST[ ] |
-//                     | [ ]GND   5V MOSI GND D0/RX1[ ] |
-//                     | [ ]Vin   [ ] [ ] [ ] D1/TX1[ ] |
-//                     |          [ ] [ ] [ ]           |
-//                     |          MISO SCK RST          |
-//                     | NANO-V3                        |
-//                     +--------------------------------+
+//                      +------------| USB |-------------+
+//                      |            +-----+             |
+// arcadeBtnsLeds[2] <- | [ ]D13/SCK/noDin   MISO/D12[ ] | -> arcadeBtnsLeds[1]
+//                      | [ ]3.3V            MOSI/D11[ ]~| -> arcadeBtnsLeds[0]
+//                      | [ ]V.ref     ___     SS/D10[ ]~| <- arcadeBtnsDin[2]
+// MATRIX_LED_PIN    <- | [ ]A0       / N \        D9[ ]~| <- arcadeBtnsDin[1]
+// PRESSURE_BTN_PIN0 -> | [ ]A1      /  A  \       D8[ ] | <- arcadeBtnsDin[0]
+// PRESSURE_BTN_PIN1 -> | [ ]A2      \  N  /       D7[ ] | <- switchboardPins[5] => 64
+// PRESSURE_LED_PIN  <- | [ ]A3       \_0_/        D6[ ]~| <- switchboardPins[4] => 32
+//     potiPins[0]   -> | [ ]A4/SDA                D5[ ]~| <- switchboardPins[3] => 16
+//     potiPins[1]   -> | [ ]A5/SCL                D4[ ] | <- switchboardPins[2] => 8
+//     potiPins[2]   -> | [ ]A6 (noDIn)       INT1/D3[ ]~| <- switchboardPins[1] => 4
+//     potiPins[3]   -> | [ ]A7 (noDIn)       INT0/D2[ ] | <- switchboardPins[0] => 2
+//                      | [ ]5V                   GND[ ] |
+//                      | [ ]RST                  RST[ ] |
+//                      | [ ]GND   5V MOSI GND D0/RX1[ ] |
+//                      | [ ]Vin   [ ] [ ] [ ] D1/TX1[ ] |
+//                      |          [ ] [ ] [ ]           |
+//                      |          MISO SCK RST          |
+//                      | NANO-V3                        |
+//                      +--------------------------------+
 //
 // Potis read 1 full counterclockwise and GAME_POTI_MAP_TO_MAX full clockwise
 //
@@ -62,7 +62,7 @@ unsigned long pressureReleaseNextStepMsec = 4000;
 /*}}}*/
 
 /* MATRIX SETTINGS {{{*/
-const int matrixDinPin = A2;
+const int matrixDinPin = A0;
 
 // 100 Leds for display (10x10) and 9 leds for pressure_release_game
 const int matrixNumLeds = 100;
